0907ed66198f1a185a892eaaf540101743c1450d
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2017-01-02 03:16:14 -0800

    1 Jan 2017: storing data in a pickled dictionary, not a CSV file, now.

--- tweet-like-trump.py
+++ tweet-like-trump.py
@@ -6,10 +6,12 @@ create a textual archive suitable for using for my Make America Sad! Again
 Twitter parody account.
 """
 
-import csv
-import time
+
+import datetime
 import glob
 import random
+import html
+import pickle
 
 import tweepy   # https://github.com/tweepy/tweepy
 
@@ -24,9 +26,50 @@ import text_handling as th              # https://github.com/patrick-brian-moone
 debugging = True
 
 base_dir = '/TrumpTweets'
+data_store = '%s/TrumpTweets_data.pkl' % base_dir
 
 markov_length = 2
 
+
+def _get_data_store():
+    """Internal function to get the entire stored data dictionary. If the data
+    storage dictionary cannot be read, create a new, empty dictionary.
+    """
+    try:
+        with open(data_store, 'rb') as the_data_file:
+            return pickle.load(the_data_file)
+    except Exception:
+        if debugging: print('WARNING: Data store does not exist, creating ...')
+        the_data = {'purpose': 'data store for TrumpTweets', 'program author': 'Patrick Mooney'}
+        with open(data_store, 'wb') as the_data_file:
+            pickle.dump(the_data, the_data_file)
+        return the_data
+
+def set_data_value(keyname, value):
+    """Store a VALUE, by KEYNAME, in the persistent data store. This data store is
+    read from disk, modified, and immediately written back to disk. This is a 
+    naive function that doesn't worry about multiple simultaneous attempts to
+    access the store: there should never be any. (This script should be the only
+    process accessing the file, and there should only be one invocation running
+    at a time.
+    """
+    the_data = _get_data_store()
+    the_data[keyname] = value
+    with open(data_store, 'wb') as the_data_file:
+        pickle.dump(the_data, the_data_file)
+
+def get_data_value(keyname):
+    """Retrieves a configuration variable from the data_store file. Not meant to be
+    easily user-editable. If the KEYNAME is not in the data store, adds a KEYNAME
+    entry with value None and returns None. If it modifies the data store in this
+    way, it then writes the data store back to disk. 
+    """
+    try:
+        return _get_data_store()[keyname]
+    except KeyError:
+        if debugging: print('WARNING: attempted to get undefined data key "%s"; initializing to None' % keyname)
+        set_data_value(keyname, None)
+        return None
 
 def get_new_tweets(screen_name='realDonaldTrump', oldest=-1):
     """Get those tweets newer than the tweet whose ID is specified as the OLDEST
@@ -53,13 +96,10 @@ def get_new_tweets(screen_name='realDonaldTrump', oldest=-1):
     return [t for t in ret if (t.id > oldest)]
 
 def get_newest_tweet_id():
-    """Get the ID of the newest tweet that appears in the tweet index, which is
-    index.csv.
+    """Get the ID of the newest tweet that has been received and massaged.
     """
     try:
-        with open('%s/tweets/index.csv' % base_dir, newline='') as csvfile:
-            csvreader = csv.reader(csvfile)
-            return max([int(r[1]) for r in list(csvreader)])
+        return get_data_value('newest_tweet_id') or -1
     except Exception:
         return -1
         
@@ -67,11 +107,9 @@ def get_last_update_date():
     """Get the last time that the database was updated.
     """
     try:
-        with open('%s/tweets/index.csv' % base_dir, newline='') as csvfile:
-            csvreader = csv.reader(csvfile)
-            #FIXME: this routine is neither complete nor called by anything
-    finally:
-        pass
+        return get_data_value('last_update_date') or datetime.datetime.min
+    except Exception:
+        return datetime.datetime.min
 
 def filter_tweet(tweet_text):
     """Returns True if the tweet should be filtered (i.e., eliminated), False if it
@@ -79,8 +117,8 @@ def filter_tweet(tweet_text):
 
     Currently, just eliminates tweets with URLs and tweets that @mention any
     account other than @realDonaldTrump (i.e., when The Donald talks about, or to,
-    anyone else) -- we don't want this account interacting with people just because
-    The Donald does. The Donald is a terrible model for appropriate behavior.
+    anyone else -- we don't want this account interacting with people just because
+    The Donald does. The Donald is a terrible model for appropriate behavior).
     
     However, there may need to be more nuanced behavior in the future.
     """
@@ -96,19 +134,29 @@ def filter_tweet(tweet_text):
 
 def normalize(the_tweet):
     """Convert THE_TWEET into a normalized form, based on the replacements in
-    SUBSTITUTION_LIST (specified below).
+    SUBSTITUTION_LIST (specified below). All substitutions are applied repeatedly,
+    in the order they appear in the list, until none of them produces a change.
+    HTML/XML entities are also unescaped, and any necessary other transformations
+    are applied.
     
     THE_TWEET is a Tweepy tweet object, not a string.
     """
-    substitution_list = [['\n', ' '],   # Newline to space
-                         ['  ', ' '],   # Two spaces to one space
-                         ]
+    substitution_list = [['\n', ' '],               # Newline to space
+                         ['  ', ' '],               # Two spaces to one space
+                         ['U.S.A.', 'U․S․A․'],      # Periods to one-dot leaders
+                         ['U. S. A.', 'U․S․A․'],    # Periods to one-dot leaders, remove spaces
+                         ['U.S.', 'U․S․'],          # Periods to one-dot leaders
+                         ['U. S.', 'U․S․'],         # Periods to one-dot leaders, remove spaces
+                         ['P․M․', 'P․M․'],          # Again
+                         ['V.P.', 'V․P․'],          # Again
+                        ]
     changed = True              # Be sure to run at least once.
     while changed:              # Repeatedly perform all substitutions until none of them change anything at all.
         orig_tweet = the_tweet.text[:]
         for which_replacement in substitution_list:
             the_tweet.text = the_tweet.text.replace(which_replacement[0], which_replacement[1])
         changed = ( orig_tweet != the_tweet.text )
+    the_tweet.text = html.unescape(the_tweet.text)    
     return the_tweet
 
 def massage_tweets(the_tweets):
@@ -120,45 +168,29 @@ def massage_tweets(the_tweets):
     return ret
 
 def save_tweets(the_tweets):
-    """Save the text from THE_TWEETS to a text file, and update the index in
-    index.csv to reference the new file created.
+    """Save the text from THE_TWEETS to a text file, and update the stored data.
 
 
     THE_TWEETS is a list of Tweets.
     """
     if len(the_tweets) == 0:        # If there are no new tweets, don't do anything
         return
-    # save the tweets
-    the_time = time.strftime("%Y-%m-%d-%H_%M_%S")
-    with open('%s/tweets/%s.txt' % (base_dir, the_time), 'w') as f:
+    with open('%s/tweets/%s.txt' % (base_dir, datetime.datetime.now().isoformat()), 'w') as f:
         f.writelines(['%s\n' % tweet.text for tweet in the_tweets])
-
-    # update the database of tweet-record filenames and ID numbers
-    try:
-        with open('%s/tweets/index.csv' % base_dir, newline='') as csvfile:
-            csvreader = csv.reader(csvfile)
-            database = list(csvreader)
-        database.append([the_time, max([t.id for t in the_tweets])])    # Add filename and its largest ID to the databaseids
-    except Exception as e:
-        database = [[the_time, max([t.id for t in the_tweets])]]
-        print('WARNING: unable to read index of stored-tweet files; downloading all and creating new list ...')
-    try:
-        with open('%s/tweets/index.csv' % base_dir, 'w', newline='') as csvfile:
-            csvwriter = csv.writer(csvfile)
-            csvwriter.writerows(database)
-    except Exception as e:
-        print('WARNING: unable to update index of stored-tweet files')
+    set_data_value('last_update_date', datetime.datetime.now())     # then, update the database of tweet-record filenames and ID numbers
 
 def update_database_if_necessary():
-    if False:        # But be more specific later.
+    if len(glob.glob('%s/tweets/*txt' % base_dir)) == 0 or (datetime.datetime.now() - get_last_update_date()).days > 9:
         t = get_new_tweets(screen_name='realDonaldTrump', oldest = get_newest_tweet_id())
         t = massage_tweets(t)
         save_tweets(t)
 
 def get_tweet(starts, the_mapping):
     the_tweet = ' ' * 160
-    while len(the_tweet) not in range(1,141):
+    while len(the_tweet) not in range(20,141):
+        if debugging: print("Generating a tweet ...", end="")
         the_tweet = sg.gen_text(the_mapping, starts, markov_length=markov_length, sentences_desired=random.choice(range(1,4)))
+        if debugging: print(" length is %d" % len(the_tweet))
     return the_tweet
     
 
