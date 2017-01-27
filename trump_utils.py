f04a0c5186bc1d47be72b2ef1156dd8f25324b71
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2017-01-27 04:13:20 -0800

    26 Jan 2017: elementary stats tracking

--- trump_utils.py
+++ trump_utils.py
@@ -22,6 +22,8 @@ donnies_tweets_dir = "%s/donnies_tweets" % data_dir
 our_minimal_tweets = '/~patrick/projects/TrumpQuiz/false_trump.csv'     # Where to export our selected tweets for the web quiz: local folder 
 donnies_minimal_tweets = '/~patrick/projects/TrumpQuiz/trump.csv'       # same, for selected tweets of Trump.
 
+answer_counts = '/~patrick/projects/TrumpQuiz/stats.csv'                # Location of file to store the answer count statistics.
+
 donnie_plaintext_tweets = '%s/donnie_plaintext_tweets.txt' % data_dir
 our_plaintext_tweets = '%s/our_plaintext_tweets.txt' % data_dir
 
@@ -46,6 +48,45 @@ def get_random_tweet(source_file):
         csvreader = csv.reader(the_archive, dialect='unix')
         return dict(zip(['text', 'id', 'date' ], random.choice(list(csvreader))))
 
+
+# This next group of functions maintains 
+def _create_stats_file():
+    """Let's hope this only ever gets called the very first time stats get counted. Otherwise, it means we've lost count and are starting over.
+    
+    For the sake of being explicit:
+          trump_right means USER CORRECTLY IDENTIFIED A QUOTE AS COMING FROM DONALD TRUMP
+          trump_wrong means USER SAID IT WAS FROM DONALD TRUMP, BUT THAT WAS WRONG (e.g., wasn't able to identify a quote from the algorithm)
+    
+    Same deal for algorithm_right and algorithm_wrong.
+    """
+    empty_counts = {
+        'trump_right': 0,
+        'trump_wrong': 0,
+        'algorithm_right': 0,
+        'algorithm_wrong': 0}
+    stats_file = open(answer_counts, 'w', newline='')
+    writer = csv.writer(stats_file)
+    for which_key in empty_counts:
+        writer.writerow([which_key, empty_counts[which_key]])
+    stats_file.close()
+
+def bump_count(which_key):
+    try:
+        fd_file = os.open(answer_counts, (os.O_RDWR | os.O_EXCL))
+    except FileNotFoundError:
+        _create_stats_file()
+        fd_file = os.open(answer_counts, (os.O_RDWR | os.O_EXCL))
+    stats_file = os.fdopen(fd_file, 'r+', newline="")
+    the_stats = {rows[0]:int(rows[1]) for rows in csv.reader(stats_file)}
+    the_stats[which_key] += 1
+    stats_file.seek(0)
+    stats_file.truncate()
+    writer = csv.writer(stats_file)
+    for which_key in the_stats:
+        writer.writerow([which_key, the_stats[which_key]])
+    stats_file.close()          # I take it we don't need to separately close the file descriptor.
+    
+    
 
 if __name__ == "__main__":
     import trump_maint as tm
